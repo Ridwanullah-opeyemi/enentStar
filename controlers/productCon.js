31bb6a54b509fc31ed2aaa724b456c5706602cf5
ridwan <ridwanullahgbolagade@gmail.com>
2025-05-08 20:14:29 +0100

    add'

--- controlers/productCon.js
+++ controlers/productCon.js
@@ -24,4 +24,46 @@ const AddProduct = async (req, res, next) => {
     }
 };
 
-module.exports = AddProduct;
+
+const GetAllProducts = async (req, res, next) => {
+    try {
+        const products = await productModel.find().sort({ createdAt: -1 }); 
+        res.status(200).json({
+            status: "success",
+            products
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const UpdateProduct = async (req, res, next) => {
+    try {
+        const { title, description } = req.body;
+        const image = req.file?.path; // only if a new image is uploaded
+
+        const updateData = { title, description };
+        if (image) updateData.image = image;
+
+        const updated = await productModel.findByIdAndUpdate(
+            req.params.id,
+            updateData,
+            { new: true }
+        );
+
+        if (!updated) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Product updated",
+            product: updated
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+module.exports = { AddProduct, GetAllProducts, UpdateProduct };
--- route/productRouter.js
+++ route/productRouter.js
@@ -1,11 +1,12 @@
-const express = require("express")
-const AddProduct = require("../controlers/productCon")
-const uploadImage = require("../middlewell/multer")
+const express = require("express");
+const { AddProduct, GetAllProducts, UpdateProduct } = require("../controlers/productCon");
+const uploadImage = require("../middlewell/multer");
 
-const productRouter = express.Router()
+const productRouter = express.Router();
 
+productRouter.post("/addproduct", uploadImage.single("image"), AddProduct);
+productRouter.get("/products", GetAllProducts); 
+productRouter.put("/update/:id", uploadImage.single("image"), UpdateProduct);
+    
 
-productRouter.post("/addproduct",uploadImage.single("image"),AddProduct)
-
-
-module.exports = productRouter
+module.exports = productRouter;
